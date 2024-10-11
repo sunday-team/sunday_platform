e40d8c291cb702e4fc35d7a85ab7c39811d7e2f4
sunday-message <ipadosipriam@icloud.com>
2024-10-11 19:25:01 +0200

    version 0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
 ## 0.0.1
 
 * Added Sunday UI package
+
+## 0.0.2
+
+* added empty example app
+
+* added file structure
---
+++ lib/MainComponents/SundayButton.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SundayButton extends StatefulWidget {
+  const SundayButton({super.key});
+
+  @override
+  State<SundayButton> createState() => _SundayButtonState();
+}
+
+class _SundayButtonState extends State<SundayButton> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
