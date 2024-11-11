213c6fd0e6539ee8c6616727b8c9df6be670bc37
sunday-message <ipadosipriam@icloud.com>
2024-11-11 18:46:12 +0100

    Fixed Sunday UI

---
+++ lib/MainComponents/sunday_layout/sunday_layout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SundayLayout extends StatefulWidget {
+  const SundayLayout({super.key});
+
+  @override
+  State<SundayLayout> createState() => _SundayLayoutState();
+}
+
+class _SundayLayoutState extends State<SundayLayout> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
