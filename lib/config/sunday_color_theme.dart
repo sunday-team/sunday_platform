b49412fe8834eeba54d58a33ac6e54ec66f89a32
Priam Chambouvet <priamc@2a02-8424-bcc3-d801-6863-4435-ab8b-d002.rev.sfr.net>
2025-03-05 16:35:20 +0100

    push to github

--- lib/config/sunday_color_theme.dart
+++ lib/config/sunday_color_theme.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
+/// A class that defines the color theme for the Sunday platform
 class SundayColorTheme {
+  /// The background color of the app
   final SColor backgroundColor;
+  
+  /// The primary text color
   final SColor textColor;
+  
+  /// The subtitle text color
   final SColor subTitleTextColor;
+  
+  /// The color used for borders
   final SColor borderColor;
+  
+  /// The color used for background elements
   final SColor backgroundElementColor;
+
+  /// Creates a [SundayColorTheme] with customizable colors
+  /// 
+  /// If any color is not provided, default values will be used
   SundayColorTheme({
     SColor? backgroundColor,
     SColor? textColor,
@@ -20,12 +33,23 @@ class SundayColorTheme {
        backgroundElementColor = backgroundElementColor ?? SColor(dark: Colors.grey[300]!, light: Colors.grey[700]!);
 }
 
+/// A color class that provides different colors for light and dark themes
 class SColor {
+  /// The color to use in light theme
   final Color light;
+  
+  /// The color to use in dark theme
   final Color dark;
   
+  /// Creates an [SColor] with required light and dark colors
   const SColor({
     required this.dark,
     required this.light
   });
+}
+
+Color sPColor(SColor sundaycolor, BuildContext context) {
+  return MediaQuery.platformBrightnessOf(context) == Brightness.dark
+      ? sundaycolor.dark
+      : sundaycolor.light;
 }
